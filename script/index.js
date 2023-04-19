acc9d73d0fc551b75bff6d52f9f0802cda848fb5
Rustambek Allaiarov <rustam20_04@mail.ru>
2023-04-20 00:38:07 +0300

    first commit Mesto

---
+++ script/index.js
@@ -0,0 +1,40 @@
+
+const popupElement = document.querySelector('.popup');
+const popupOpenButtonElement = document.querySelector('.profile__edit-btn');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+
+
+const togglePopupVisibility = function () {
+    console.log('togglePopupVisibility');
+    popupElement.classList.toggle('popup_is-opened')
+}
+
+popupOpenButtonElement.addEventListener('click', open);
+popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
+
+const openPopup = function () {
+    console.log('openPopup');
+    popupElement.classList.add('popup_is-opened');
+}
+
+const closePopup = function () {
+    console.log('closePopup');
+    popupElement.classList.remove('popup_is-opened');
+}
+
+const closePopupByClickOnOverlay = function (event) {
+    if (event.target !== event.currentTarget) return;
+
+    closePopup();
+}
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay)
+
+const headerElement = document.querySelector('.header');
+headerElement.addEventListener('click', () => {
+    console.log('HEADER CLICKED');
+}, {
+    once: true,
+    capture: true,
+})
