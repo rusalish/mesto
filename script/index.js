0453933bf5235c424a7a7df86128161d321603a2
Rustambek Allaiarov <rustam20_04@mail.ru>
2023-04-22 14:22:59 +0300

    Ready to check

--- script/index.js
+++ script/index.js
@@ -2,37 +2,34 @@
 const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit-btn');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const popupInputElement = document.querySelector('.popup__input')
-
-console.log(popupInputElement)
+const profileNameElement = document.querySelector('.profile__name');
+const profileOccupationElement = document.querySelector('.profile__occupation');
+const popupInputNameElement = document.querySelector('.popup__input_type_name');
+const popupInputOccupationElement = document.querySelector('.popup__input_type_occupation');
+const popupFormElement = document.querySelector('.profile-form');
+const popupSubmitElement = document.querySelector('.popup__save-btn')
 
 
 const togglePopupVisibility = function () {
     console.log('togglePopupVisibility');
-    popupElement.classList.toggle('popup_is-opened')
+    popupElement.classList.toggle('popup_is_opened')
 }
-
-popupOpenButtonElement.addEventListener('click', open);
-popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
 
 const openPopup = function () {
     console.log('openPopup');
-    popupElement.classList.add('popup_is-opened');
+    popupElement.classList.add('popup_is_opened');
+    popupInputNameElement.value = profileNameElement.textContent;
+    popupInputOccupationElement.value = profileOccupationElement.textContent;
 }
 
 const closePopup = function () {
     console.log('closePopup');
-    popupElement.classList.remove('popup_is-opened');
+    popupElement.classList.remove('popup_is_opened');
 }
 
 const closePopupByClickOnOverlay = function (event) {
     if (event.target !== event.currentTarget) return;
-
     closePopup();
-}
-function handlePopupEditOpen() {
-
-    openPopup()
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
